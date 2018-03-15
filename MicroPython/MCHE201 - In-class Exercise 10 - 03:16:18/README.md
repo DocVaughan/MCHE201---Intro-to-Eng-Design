1ec123c1d215b823ec191388bd5d18361eea3044
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2018-03-15 15:19:15 -0500

    Added in-class exercised 9 and 10

---
+++ MicroPython/MCHE201 - In-class Exercise 10 - 03:16:18/README.md
@@ -0,0 +1,20 @@
+This script is one way to solve the 10th in-class exercise from MCHE201 in the 
+spring semester of 2018. 
+
+That exercise was given as:
+* Connect
+  - a pushbutton
+  - the servomotor
+* Start the servo at 0deg
+* When the pushbutton is pressed, move the servo
+* Only allow this to happen once per 30seconds
+
+In this version of the solution, check for the status of the button 
+repeatedly. Once it is pressed, we move the servo then sleep for 30s. This is
+not the most generalizable way to do this, but it does solve the problem at 
+hand. 
+
+To generalize, you need to allow for other operations during the 30 seconds.
+These could happen inside the "button pressed" part of the code, in place of
+the pure sleep. For the final contest, you might still want long-ish delay
+here to prevent your machine from re-running if a trial 
--- MicroPython/MCHE201 - In-class Exercise 9 - 03:16:18/main.py
+++ MicroPython/MCHE201 - In-class Exercise 9 - 03:16:18/main.py
@@ -1,7 +1,7 @@
 ###############################################################################
 # main.py
 #
-# This script is one way to solve the 8th in-class exercise from MCHE201 in the 
+# This script is one way to solve the 9th in-class exercise from MCHE201 in the 
 # spring semester of 2018. In this version of the solution, we use nested loops
 # check for the status of the button. To prevent multiple readings of the
 # same button press, we have to be careful with the timing. Even so, this 
@@ -10,16 +10,19 @@
 # this solution to be more robust by changing the values in the various 
 # time.sleep_ms() commands.
 # 
+# We keep track the time elapsed since the first press of the button and turn
+# on an LED every second until the 5s limit is up. So, the LEDs serve as a 
+# progress bar on the timer.
+# 
 # That exercise was given as:
 # * Connect a pushbutton
 # * Turn on the green LED
-# * When the pushbutton is pressed
-#     - Turn on the red LED
-#     - Turn off the green LED
-# * When the button is pressed again
-#     - Turn off the red LED
-#     - Turn on the green LED
-#     - Print the time elapsed between button pressed to the REPL
+# * Once the button is pressed the first time, turn off all LEDs. 
+# * Then, turn on 1 LED every 10s until the button is pressed again
+# * When the button is pressed again, print the time elapsed between button pressed to the REPL
+# * If more than 5s elapses:
+#     - Print "You took too long!!!" to the REPL
+#     - Turn on only the green LED again
 #
 # Created: 03/13/18
 #   - Joshua Vaughan
@@ -39,6 +42,8 @@ import time # import the time module
 # Assign the names to the onboard LEDs
 RED_LED = pyb.LED(1)
 GREEN_LED = pyb.LED(2)
+YELLOW_LED = pyb.LED(3)
+BLUE_LED = pyb.LED(4)
 
 # Assign the input pin to variable input_pin
 # We set it up as an input with a pulldown resistor
@@ -55,7 +60,6 @@ while (True):
     if (input_state):
         start_time = time.ticks_ms() # save the current time
         GREEN_LED.off()
-        RED_LED.on()
         print("Button pressed to start.\n")
 
         # We could also have another delay here, to force a longer separation 
@@ -73,21 +77,22 @@ while (True):
             # Doing so will help "debounce" the switch. For more information on 
             # debouncing switches see:
             #  http://docs.micropython.org/en/latest/pyboard/pyboard/tutorial/debounce.html
-            time.sleep_ms(10) 
+            time.sleep_ms(100) 
 
             # read the state of the input
             input_state = input_pin.value()
+            
+            # Calculate the time elapsed in ms since we first pressed the button
+            time_elapsed = time.ticks_diff(time.ticks_ms(), start_time)
 
             if (input_state):
-                end_time = time.ticks_ms() # save the current time
                 GREEN_LED.on()
                 RED_LED.off()
+                YELLOW_LED.off()
+                BLUE_LED.off()
                 print("Button pressed to stop.")
-
-                # Now, calculate the elapsed time in ms and print it out.
-                time_elapsed = time.ticks_diff(end_time, start_time)
-                print("Elapsed ticks = {}\n".format(time_elapsed))
-
+                print("Elapsed time = {}ms\n".format(time_elapsed))
+                
                 # Set button_timing to False because we are no longer in a 
                 # timing part of the algorithm. This will break the while loop
                 # that is looking for the 2nd press of the button, therefore
@@ -98,5 +103,27 @@ while (True):
                 # separation between the pressing of the button to stop the 
                 # timer and pressing the button to start the timer again.
                 time.sleep_ms(200)
+            else:
+                # print("Elapsed ticks = {}ms\n".format(time_elapsed))
+                if time_elapsed > 5000: # >5000ms
+                    print("You took too long!!!")
+                    
+                    # We no longer want to look for the "timing" button press
+                    button_timing = False
+                    
+                    # Turn off the LEDs
+                    RED_LED.off()
+                    GREEN_LED.off()
+                    YELLOW_LED.off()
+                    BLUE_LED.off()
+                    
+                elif time_elapsed > 4000: # >4000ms
+                    BLUE_LED.on() 
+                elif time_elapsed > 3000: # >3000ms
+                    YELLOW_LED.on()   
+                elif time_elapsed > 2000: # > 2000ms 
+                    GREEN_LED.on()
+                elif time_elapsed > 1000: # > 1000ms
+                    RED_LED.on()
 
     time.sleep_ms(10)          # Sleep 10 milliseconds (0.1s)
