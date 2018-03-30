c83e748e3f468ac7593b3e72bd378663ab45db6b
Joshua Vaughan <joshua.vaughan@louisiana.edu>
2018-03-30 11:17:43 -0500

    Added code to control two DC motors with TB6612 motor drivers
    This code includes both MicroPython code and Fritzing drawings. The
    drawings include both the Adafruit and Sparkfun versions of the TB6612
    breakout boards. There are both "direct" control of the pyboard pins in
    the main script and use of `TB6612.py` which contains a convenience
    class for the driver that abstracts away most of the low-level control.

--- MicroPython/pyboard TB6612 with import two motors/main.py
+++ MicroPython/pyboard TB6612 with import two motors/main.py
@@ -7,12 +7,19 @@
 #  * https://www.adafruit.com/product/2448
 #  * https://www.sparkfun.com/products/14451
 #
-# Hardware Connection
+# In this version, we use a class developed specifically for interfacing with
+# and controlling the TB6612. It provides several convenience functions. To
+# use this class, you need to copy the TB6612.py file to the pyboard.
+#
+# Hardware Connections
 # pyboard | TB6612 Breakout 
 # ------- | ----------------------------
 # X6      | PWMA 
 # X7      | AIN2
-# X8      | AIN 1
+# X8      | AIN1
+# Y4      | PWMB
+# Y5      | BIN2
+# Y6      | BIN1
 # 3V3     | Vcc
 # GND     | GND
 # -       | STBY to VCC via 10K resistor
@@ -20,15 +27,14 @@
 # Motor driver shec sheet - https://cdn-shop.adafruit.com/datasheets/TB6612FNG_datasheet_en_20121101.pdf
 # Adafruit Overview - https://learn.adafruit.com/adafruit-tb6612-h-bridge-dc-stepper-motor-driver-breakout
 # SparkFun Overview - https://learn.sparkfun.com/tutorials/tb6612fng-hookup-guide
-#
-# Created: 11/01/17
+# 
+# Created: 03/30/18
 #   - Joshua Vaughan
 #   - joshua.vaughan@louisiana.edu
 #   - http://www.ucs.louisiana.edu/~jev9637
 #
 # Modified:
-#   * 03/19/18 - JEV - joshua.vaughan@louisiana.edu
-#       - Added SparkFun links and connections tables
+#   *
 #
 # TODO:
 #   * 
@@ -36,18 +42,31 @@
 
 import pyb  # import the pyboard module
 import time # import the time module
+import TB6612 # import the file containing our TB6612 motor code
 
-# We need to set up two digital outputs that we will use to control the 
-# direction of the motor.
-A1_pin = pyb.Pin("X8", pyb.Pin.OUT_PP, pull=pyb.Pin.PULL_DOWN)
-A2_pin = pyb.Pin("X7", pyb.Pin.OUT_PP, pull=pyb.Pin.PULL_DOWN)
+# We need to set up two digital outputs for each motor  that we will use to 
+# control the direction of the motor. In this case, the TB6612 class file will 
+# handle the low-level settings for the pins
+A1_PIN = "X8"
+A2_PIN = "X7"
+B1_PIN = "Y6"
+B2_PIN = "Y5"
 
-# We also need to set up a third pin to control the speed of the motor. This 
+# We also need to set up a third pin to control the speed of each motor. This 
 # output needs to have PWM capabilities, so it should be connected to a pin
-# that has an associated timer on the pyboard
-PWM_pin = pyb.Pin("X6")
-PWM_TIMER = pyb.Timer(2, freq=20000)
-PWM_CHANNEL = PWM_TIMER.channel(1, pyb.Timer.PWM, pin=PWM_pin)
+# that has an associated timer on the pyboard. If these are defined correctly
+# here, then the TB6612 class file will handle the low-level set up
+PWMA_PIN = "X6"
+PWMA_TIMER = 2
+PWMA_CHANNEL = 1
+
+PWMB_PIN = "Y4"
+PWMB_TIMER = 4
+PWMB_CHANNEL = 4
+
+# Now, we can create a motor instance, calling upon the TB6612 class
+motorA = TB6612.motor(PWMA_PIN, A1_PIN, A2_PIN, PWMA_TIMER, PWMA_CHANNEL)
+motorB = TB6612.motor(PWMB_PIN, B1_PIN, B2_PIN, PWMB_TIMER, PWMB_CHANNEL)
 
 
 try:
@@ -55,42 +74,38 @@ try:
     for counter in range(5):
         print("Turning on approx 1/2 speed")
 
-        # to turn the motor in one direction, A1 should be high and A2 low
-        A1_pin.value(1)
-        A2_pin.value(0)
-    
-        # We set the speed by setting the duty cycle of the PWM command
-        PWM_CHANNEL.pulse_width_percent(50)
+        # To start the motor, issue the .set_speed() command with a speed 
+        # between -100 and 100
+        motorA.set_speed(50)
+        motorB.set_speed(50)
     
         # Sleep 2 seconds. The motor should keep running
         time.sleep(2)
     
         print("Stopping...")
-        # To stop the motor, we should set the duty cycle to 0
-        PWM_CHANNEL.pulse_width_percent(0)
+        # To stop the motor, we should set tell it to stop
+        motorA.stop()
+        motorB.stop()
         time.sleep_ms(100)         # Sleep briefly to let the motor stop
     
-    
         print("Turning on approx 1/2 speed in the other direction")
-        # to turn the motor in one direction, A0 should be high and A1 low
-        A1_pin.value(0)
-        A2_pin.value(1)
-    
-        # We set the speed by setting the duty cycle of the PWM command
-        PWM_CHANNEL.pulse_width_percent(50)
+        # To move in the opposite direction, issue a negative speed       
+        motorA.set_speed(-50)
+        motorB.set_speed(-50)
     
         # Sleep 2 seconds. The motor should keep running
         time.sleep(2)
     
         print("Stopping...")
-        # To stop the motor, we should set the duty cycle to 0
-        PWM_CHANNEL.pulse_width_percent(0)
+        motorA.stop()
+        motorB.stop()
         time.sleep_ms(100)         # Sleep briefly to let the motor stop
 
 except:
-    print("\n\nSome Error Occurred. Stopping motor.\n\n")
+    print("Some Error Occurred. Stopping motor.")
     # To stop the motor, we should set the duty cycle to 0
-    PWM_CHANNEL.pulse_width_percent(0)
+    motorA.stop()
+    motorB.stop()
     
     # If we call raise here, we'll still get the information on why the 
     # exception was raised in the first place. Without this, we do not.
